2928b992f1970749193e477d3bbe82e768c034ad
Jattzz <108416182+jattzz@users.noreply.github.com>
2023-11-29 03:33:45 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    { 
+      numb: 1,
+      question: "Which anime is this charater from?",
+      answer: "B. Kimetsu No Yaiba",
+      options: [
+          "A. Naruto Shippuden",
+          "B. Kimetsu No Yaiba",
+          "C. Seihei Mato No Slave",
+          "D. Boku No Pico"
+        ],
+    },
+    {
+      numb: 2,
+      question: "What's the name of this character?",
+      answer: "C. Dabi",
+      options: [
+          "A. Deku",
+          "B. Goku",
+          "C. Dabi",
+          "D. Daki"
+        ],
+    },
+    {
+      numb: 3,
+      question: "Which alchemist is this character?",
+      answer: "D. Fullmetal Alchemist",
+      options: [
+          "A. Crimson Alchemist",
+          "B. Fire Alchemist",
+          "C. Blood Alchemist",
+          "D. Fullmetal Alchemist"
+        ],
+    },
+    {
+      numb: 4,
+      question: "The main characters of Kimi No Nawa were ______",
+      answer: "B. Taki and Mitsuha",
+      options: [
+          "A. Edward and Winry",
+          "B. Taki and Mitsuha",
+          "C. Naruto and Hinata",
+          "D. Hotarou and Chitanda"
+        ],
+    },
+    {
+      numb: 5,
+      question: "What's the name of this character and his Kasugai crow?",
+      answer: "A. Agatsuma Zenitsu and Chuntarou",
+      options: [
+          "A. Agatsuma Zenitsu and Chuntarou",
+          "B. Hashibira Zanitsu and Gyuutaro",
+          "C. Agatsuma Zenitsu and Gyuutaro",
+          "D. Hashibira Zenitsu and Gonpachiro"
+        ],
+    },
+];
